0d0b7955f3fe643ed447b93175fa2ff5767b6c0b
Ian Glenn Curilan <iancurilan1027.com>
2025-04-25 00:18:33 +0800

    feat: navbar color added

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -12,9 +12,7 @@ function toggleTheme() {
   <v-responsive class="border rounded">
     <v-app :theme="theme">
       <!-- App Bar -->
-      <v-app-bar
-        color="teal-darken-4"
-        image="https://plus.unsplash.com/premium_photo-1675362696408-19eb8ba38823?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      <v-app-bar style="background-color: #328E6E;"
       >
         <template v-slot:image>
         </template>
@@ -48,3 +46,4 @@ function toggleTheme() {
     </v-app>
   </v-responsive>
 </template>
+
