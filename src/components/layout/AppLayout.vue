4b68aacc0495180068265ddfb912cb6a281678a7
Ian Glenn Curilan <iancurilan1027.com>
2025-04-25 00:06:17 +0800

    feat: color green added

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -13,7 +13,9 @@ function toggleTheme() {
     <v-app :theme="theme">
       <!-- App Bar -->
       <v-app-bar
-       >
+        color="teal-darken-4"
+        image="https://plus.unsplash.com/premium_photo-1675362696408-19eb8ba38823?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      >
         <template v-slot:image>
         </template>
 
