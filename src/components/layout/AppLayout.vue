e04adde5f2c0d1a2663f31287175bc3169cba756
Ian Glenn Curilan <iancurilan1027.com>
2025-04-24 23:48:33 +0800

    refactor: remove app bar properties and image slot

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -13,11 +13,8 @@ function toggleTheme() {
     <v-app :theme="theme">
       <!-- App Bar -->
       <v-app-bar
-        color="teal-darken-4"
-        image="https://plus.unsplash.com/premium_photo-1675362696408-19eb8ba38823?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      >
+       >
         <template v-slot:image>
-          <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
         </template>
 
         <v-app-bar-title>Quickwork</v-app-bar-title>
