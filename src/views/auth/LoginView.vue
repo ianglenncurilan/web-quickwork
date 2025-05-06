1a27d916988bc8e9dd019091716040f68461daa9
Ian Glenn Curilan <iancurilan1027.com>
2025-05-06 19:17:55 +0800

    fix: fixed the background missing

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -190,7 +190,7 @@ function handleLogin() {
   padding: 2rem;
   border-radius: 12px; /* Rounded corners */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Shadow effect */
-  background-image: url('@/assets/background-pattern.png'); /* Background image */
+  background-image: url('@/assets/qwkwrk.png');
   background-size: cover;
   background-position: center;
   text-align: center;
